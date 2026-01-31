e798b026bdc055c4457a85419588ba0252aa75db
Davii-Ng <nminhd98@gmail.com>
2026-01-31 00:54:26 -0500

    document load implemented

--- gemini.py
+++ gemini.py
@@ -8,6 +8,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import cosine_distances
 from sklearn.metrics.pairwise import manhattan_distances
 from sklearn.metrics.pairwise import euclidean_distances
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from transformers import GPT2Tokenizer
+
 
 
 class RAG_pipeline():
@@ -23,37 +27,49 @@ def main():
     load_dotenv()
     
     GEMINI_API = os.getenv("GEMINI_API_KEY")
-    CHROMA_API = os.getenv("CHROMA_API_KEY")
+    # CHROMA_API = os.getenv("CHROMA_API_KEY")
 
-    model = ChatGoogleGenerativeAI(
-        model="gemini-3-flash-preview",
-        temperature=1.0,  
-        max_tokens=None,
-        timeout=None,
-        max_retries=2,
-        api_key = GEMINI_API
-    )
+    # model = ChatGoogleGenerativeAI(
+    #     model="gemini-3-flash-preview",
+    #     temperature=1.0,  
+    #     max_tokens=None,
+    #     timeout=None,
+    #     max_retries=2,
+    #     api_key = GEMINI_API
+    # )
 
-    messages = [
-        (
-            "system",
-            "You are a helpful assistant that translates English to French. Translate the user sentence.",
-        ),
-        ("human", "I love programming."),
-    ]
+    # messages = [
+    #     (
+    #         "system",
+    #         "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    #     ),
+    #     ("human", "I love programming."),
+    # ]
 
     
-    # ai_msg = model.invoke(messages)
-    # print(ai_msg.text)
+    # # ai_msg = model.invoke(messages)
+    # # print(ai_msg.text)
+
+    loader = PyPDFLoader(
+    "./Multi-RAG.pdf",
+    mode="single",
+    pages_delimiter="\n-------THIS IS A CUSTOM END OF PAGE-------\n",
+)
+    docs = loader.load()
+
+
+    
+
+    text_splitter = RecursiveCharacterTextSplitter( chunk_size=1000, chunk_overlap=200)
+    texts = text_splitter.split_text(docs[0].page_content)
+
 
 
     query_embeddings = GoogleGenerativeAIEmbeddings(model = "models/gemini-embedding-001", task_type = "RETRIEVAL_QUERY")
     doc_embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001", task_type="RETRIEVAL_DOCUMENT")
 
-    q_embed = query_embeddings.embed_query("What is the capital of France?")
-    d_embed = doc_embeddings.embed_documents(
-    ["The capital of France is Paris.", "Philipp is likes to eat pizza."]
-)
+    q_embed = query_embeddings.embed_query("What is Multi-RAG?")
+    d_embed = doc_embeddings.embed_documents(texts)
 
     for i, d in enumerate(d_embed):
         print(f"Document {i + 1}:")
