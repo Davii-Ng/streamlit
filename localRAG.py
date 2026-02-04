74a33bad4743adcce1ff5a5a79a9f77d8eabbe34
Davii-Ng <nminhd98@gmail.com>
2026-02-04 01:29:18 -0500

    Recursive Text Splitter implementation complete

--- localRAG.py
+++ localRAG.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
+from transformers import GPT2TokenizerFast
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter, RecursiveCharacterTextSplitter
+
 
 
 
@@ -30,15 +33,48 @@ def load_docs(pdf_path):
 
 
 
-def text_embedding():
+def Recursive_Text_Splitter(document, chunk_size, chunk_overlap):
+    """Traditional Text Splitters"""
+
+    text_splitter = RecursiveCharacterTextSplitter( chunk_size = chunk_size, chunk_overlap = chunk_overlap, add_start_index = True)
+    texts = text_splitter.split_documents(document)
+
+    return texts
+
+
+
+
+
+def Tokenizer_Text_Splitter(document, chunk_size, chunk_overlap):
+    """Split text using Tokens via HuggingFace's Transformer tokenizer"""
+
+    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+    text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(
+    tokenizer, chunk_size= chunk_size, chunk_overlap= chunk_overlap
+)
+    text = text_splitter.split(document)
+    return text
+
+
+
+def text_embedding(documents, embedding_model):
     """Generate embeddings from """
     return
 
 
 def main():
+    load_dotenv()
+    embedding_model = os.getenv("EMBEDDING_MODEL")
+
 
     pdf_folder_path = "./data"
     docs = load_docs(pdf_folder_path)
+
+    text = Recursive_Text_Splitter(docs, 1000, 200)
+
+    print(text)
+
+
 
     
 
